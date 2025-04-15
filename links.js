db164b5fd1d169891448716e00540d53bdf0da29
mbachem <mr.bachem@gmail.com>
2025-04-15 23:31:46 +0200

    Aikido-Schule Bodo Rödel, 5. Kyu-Prüfungsprogramm
    https://www.youtube.com/watch?v=O7C433RP5Oc

--- links.js
+++ links.js
@@ -1,4 +1,32 @@
 let vidurl = {
+
+  3 : {
+    21 : {
+        "url" : "https://www.youtube.com/watch?v=O7C433RP5Oc&t=105",
+        "label" : "suwari waze - ryote dori - kokyo ho"
+      }
+  },
+
+  12 : {
+    1 : {
+      "url" : "https://www.youtube.com/watch?v=O7C433RP5Oc",
+      "label" : "tachi waza - ai hanmi katate dori - ikkyo"
+    },
+    8 : {
+      "url" : "https://www.youtube.com/watch?v=O7C433RP5Oc&t=51",
+      "label" : "tachi waza - ai hanmi katate dori - kote gaeshi"
+    },
+    9 : {
+      "url" : "https://www.youtube.com/watch?v=O7C433RP5Oc&t=21",
+      "label" : "tachi waza - ai hanmi katate dori - shiho nage"
+    },
+    10 : {
+      "url" : "https://www.youtube.com/watch?v=O7C433RP5Oc&t=42",
+      "label" : "tachi waza - ai hanmi katate dori - irimi nage"
+    }
+
+  },
+
   11 : {
     1 : {
       "url" : "https://www.youtube.com/watch?v=GC2hnFXAhOY&t=811s",
@@ -10,10 +38,26 @@ let vidurl = {
     },
   },
 
+  20 : {
+    1: {
+      "url" : "https://www.youtube.com/watch?v=O7C433RP5Oc&t=63",
+      "label" : "tachi waza - shomen ushi - ikkyo"
+    },
+    8: {
+      "url" : "https://www.youtube.com/watch?v=O7C433RP5Oc&t=93",
+      "label" : "tachi waza - shomen ushi - kote gaeshi"
+    },
+    10: {
+      "url" : "https://www.youtube.com/watch?v=O7C433RP5Oc&t=83",
+      "label" : "tachi waza - shomen ushi - irimi nage"
+    }
+  },
+
   21 : {
     10 : {
       "url" : "https://www.youtube.com/watch?v=3NGQz--VK0E&t=154",
       "label" : "yokomen uchi - irimi nage"
     }
   }
+
 };
