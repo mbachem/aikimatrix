772fddc8f02a62a48bf6ac092f50b6e991bcaa29
mbachem <mr.bachem@gmail.com>
2025-04-16 06:53:03 +0200

    Aikido-Schule Bodo Rödel, 4. Kyu-Prüfungsprogramm
    https://www.youtube.com/watch?v=QPbWxeMZO-E

--- links.js
+++ links.js
@@ -1,62 +1,154 @@
 let vidurl = {
 
+  1 : {
+    1 : {
+      "label" : "suwari waza - ai hanmi katate dori - ikyo",
+      "url" : "https://www.youtube.com/watch?v=QPbWxeMZO-E&t=236s"
+    },
+    10 : {
+      "label" : "suwari waza - ai hanmi katate dori - irimi nage",
+      "url" : "https://www.youtube.com/watch?v=QPbWxeMZO-E&t=255s"
+    }
+  },
+
+  6 : {
+    1 : {
+      "label" : "suwari waza - shomen uchi - ikyo",
+      "url" : "https://www.youtube.com/watch?v=QPbWxeMZO-E&t=262s"
+    },
+    10 : {
+      "label" : "suwari waza - shomen uchi - irimi nage",
+      "url" : "https://www.youtube.com/watch?v=QPbWxeMZO-E&t=280s"
+    },
+  },
+
   3 : {
     21 : {
-        "url" : "https://www.youtube.com/watch?v=O7C433RP5Oc&t=105",
-        "label" : "suwari waze - ryote dori - kokyo ho"
+        "label" : "suwari waze - ryote dori - kokyo ho",
+        "url" : "https://www.youtube.com/watch?v=O7C433RP5Oc&t=105s"
       }
   },
 
   12 : {
     1 : {
-      "url" : "https://www.youtube.com/watch?v=O7C433RP5Oc",
-      "label" : "tachi waza - ai hanmi katate dori - ikkyo"
+      "label" : "tachi waza - ai hanmi katate dori - ikyo",
+      "url" : "https://www.youtube.com/watch?v=O7C433RP5Oc"
+    },
+    2 : {
+      "label" : "tachi waza - ai hanmi katate dori - nikyo",
+      "url" : "https://www.youtube.com/watch?v=QPbWxeMZO-E"
     },
     8 : {
-      "url" : "https://www.youtube.com/watch?v=O7C433RP5Oc&t=51",
-      "label" : "tachi waza - ai hanmi katate dori - kote gaeshi"
+      "label" : "tachi waza - ai hanmi katate dori - kote gaeshi",
+      "url" : "https://www.youtube.com/watch?v=O7C433RP5Oc&t=51s"
     },
     9 : {
-      "url" : "https://www.youtube.com/watch?v=O7C433RP5Oc&t=21",
-      "label" : "tachi waza - ai hanmi katate dori - shiho nage"
+      "label" : "tachi waza - ai hanmi katate dori - shiho nage",
+      "url" : "https://www.youtube.com/watch?v=O7C433RP5Oc&t=21s"
     },
     10 : {
-      "url" : "https://www.youtube.com/watch?v=O7C433RP5Oc&t=42",
-      "label" : "tachi waza - ai hanmi katate dori - irimi nage"
-    }
+      "label" : "tachi waza - ai hanmi katate dori - irimi nage",
+      "url" : "https://www.youtube.com/watch?v=O7C433RP5Oc&t=42s"
+    },
+    15 : {
+      "label" : "tachi waza - ai hanmi katate dori - ude kime nage",
+      "url" : "https://www.youtube.com/watch?v=QPbWxeMZO-E&t=24s"
+    },
 
+  },
+
+
+  13 : {
+    1 : {
+      "label" : "tachi waza - gyaku hanmi katate dori - ikyo",
+      "url" : "https://www.youtube.com/watch?v=QPbWxeMZO-E&t=39s"
+    },
+    2 : {
+      "label" : "tachi waza - gyaku hanmi katate dori - nikyo",
+      "url" : "https://www.youtube.com/watch?v=QPbWxeMZO-E&t=57s"
+    },
+    8 : {
+      "label" : "tachi waza - gyaku hanmi katate dori - shiho nage",
+      "url" : "https://www.youtube.com/watch?v=QPbWxeMZO-E&t=98s"
+    },
+    9 : {
+      "label" : "tachi waza - gyaku hanmi katate dori - shiho nage",
+      "url" : "https://www.youtube.com/watch?v=QPbWxeMZO-E&t=84s"
+    },
+    10 : {
+      "label" : "tachi waza - gyaku hanmi katate dori - irimi nage",
+      "url" : "https://www.youtube.com/watch?v=QPbWxeMZO-E&t=111s"
+    },
+    11 : {
+      "label" : "tachi waza - gyaku hanmi katate dori - sokumen irimi nage",
+      "url" : "https://www.youtube.com/watch?v=QPbWxeMZO-E&t=147s"
+    },
+    12 : {
+      "label" : "tachi waza - gyaku hanmi katate dori - tenchi nage",
+      "url" : "https://www.youtube.com/watch?v=QPbWxeMZO-E&t=121s"
+    },
+    13 : {
+      "label" : "tachi waza - gyaku hanmi katate dori - uchi kaiten nage",
+      "url" : "https://www.youtube.com/watch?v=QPbWxeMZO-E&t=128s"
+    },
+    15 : {
+      "label" : "tachi waza - gyaku hanmi katate dori - ude kime nage",
+      "url" : "https://www.youtube.com/watch?v=QPbWxeMZO-E&t=136s"
+    },
   },
 
   11 : {
     1 : {
-      "url" : "https://www.youtube.com/watch?v=GC2hnFXAhOY&t=811s",
-      "label" : "hanmi handachi waza - ushiro ryō kata dori - Ikkyo"
+      "label" : "hanmi handachi waza - ushiro ryō kata dori - ikyo",
+      "url" : "https://www.youtube.com/watch?v=GC2hnFXAhOY&t=811s"
     },
     2 : {
-      "url" : "https://www.youtube.com/watch?v=GC2hnFXAhOY&t=824s",
-      "label" : "hanmi handachi waza - ushiro ryō kata dori - Nikkyo"
+      "label" : "hanmi handachi waza - ushiro ryō kata dori - nikyo",
+      "url" : "https://www.youtube.com/watch?v=GC2hnFXAhOY&t=824s"
+    },
+  },
+
+  14 : {
+    12 : {
+      "label" : "tachi waza - ryote dori - tenchi nage",
+      "url" : "https://www.youtube.com/watch?v=QPbWxeMZO-E&t=201s"
+    }
+  },
+
+  16 : {
+    1 : {
+      "label" : "tachi waza - kata dori - ikyo",
+      "url" : "https://www.youtube.com/watch?v=QPbWxeMZO-E&t=154s"
+    },
+    2 : {
+      "label" : "tachi waza - kata dori - nikyo",
+      "url" : "https://www.youtube.com/watch?v=QPbWxeMZO-E&t=175s"
     },
   },
 
   20 : {
     1: {
-      "url" : "https://www.youtube.com/watch?v=O7C433RP5Oc&t=63",
-      "label" : "tachi waza - shomen ushi - ikkyo"
+      "label" : "tachi waza - shomen uchi - ikyo",
+      "url" : "https://www.youtube.com/watch?v=O7C433RP5Oc&t=63s"
+    },
+    2: {
+      "label" : "tachi waza - shomen uchi - nikyo",
+      "url" : "https://www.youtube.com/watch?v=QPbWxeMZO-E&t=208s"
     },
     8: {
-      "url" : "https://www.youtube.com/watch?v=O7C433RP5Oc&t=93",
-      "label" : "tachi waza - shomen ushi - kote gaeshi"
+      "label" : "tachi waza - shomen uchi - kote gaeshi",
+      "url" : "https://www.youtube.com/watch?v=O7C433RP5Oc&t=93s"
     },
     10: {
-      "url" : "https://www.youtube.com/watch?v=O7C433RP5Oc&t=83",
-      "label" : "tachi waza - shomen ushi - irimi nage"
+      "label" : "tachi waza - shomen uchi - irimi nage",
+      "url" : "https://www.youtube.com/watch?v=O7C433RP5Oc&t=83s"
     }
   },
 
   21 : {
     10 : {
-      "url" : "https://www.youtube.com/watch?v=3NGQz--VK0E&t=154",
-      "label" : "yokomen uchi - irimi nage"
+      "label" : "tachi waza - yokomen uchi - irimi nage",
+      "url" : "https://www.youtube.com/watch?v=3NGQz--VK0E&t=154s"
     }
   }
 
--- matrix.js
+++ matrix.js
@@ -1,31 +1,48 @@
+function clearHighlight($highlighed) {
+  $highlighed.removeClass("kyu-1").removeClass("kyu-2").removeClass("kyu-3").removeClass("kyu-4").removeClass("kyu-5");
+}
+
 $(function() {
   $('table#matrix td.kyu').hover(function() {
+    let kyu = $(this).data("kyu");
 
-    $('tr.angriff td').add("td.technik").removeClass("selected");
+    clearHighlight($('tr.angriff td').add("td.technik"));
 
     let angriff = Number($(this).data("angriff"));
-    $('tr.angriff td:nth-child(' + angriff + ')').addClass('selected');
+    $('tr.angriff td:nth-child(' + angriff + ')').addClass('kyu-' + kyu);
 
     let technik = Number($(this).data("technik"));
-    $("td.technik-" + technik).addClass('selected');
+    $("td.technik-" + technik).addClass('kyu-' + kyu);
 
-    if ((typeof(vidurl[angriff]) === "undefined") || (typeof(vidurl[angriff][technik]) === "undefined")) {
+    if (
+      (typeof(vidurl[angriff]) === "undefined") ||
+      (typeof(vidurl[angriff][technik]) === "undefined")
+    ) {
       console.log("missing video Link fuer Angiff " + angriff + " und Technik " + technik);
     }
-
   });
 
   $('td.empty').hover(function() {
-    $('tr.angriff td').add("td.technik").removeClass("selected");
+    clearHighlight($('tr.angriff td').add("td.technik"));
   });
 
   $('table#matrix td.kyu').each(function() {
     let angriff = Number($(this).data("angriff"));
     let technik = Number($(this).data("technik"));
     let kyu = $(this).html();
-    if ((typeof(vidurl[angriff]) !== "undefined") && (typeof(vidurl[angriff][technik]) !== "undefined")) {
+    $(this).data("kyu", kyu);
+
+    if (
+      (typeof(vidurl[angriff]) !== "undefined") &&
+      (typeof(vidurl[angriff][technik]) !== "undefined")
+    ) {
       $(this).html(
-        '<a href="' + vidurl[angriff][technik]["url"] + '" title="' + vidurl[angriff][technik]["label"] + '" target="_blank">' + kyu + "</a>"
+        '<a ' + 
+          'href="' + vidurl[angriff][technik]["url"] + '" ' + 
+          'title="' + vidurl[angriff][technik]["label"] + '" ' + 
+          'target="_blank">' +
+          kyu +
+        '</a>'
       );
     }
   });
